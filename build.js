2d905b8dac5a9d8fbafe3625f70a01b293635162
Kyle Powers <kylepowers@gmail.com>
2014-12-01 05:23:56 -0600

    fix build script

--- build.js
+++ build.js
@@ -4,23 +4,15 @@ var nw = new NwBuilder({
   platforms: ['osx', 'win', 'linux32', 'linux64'],
   version: '0.11.2',
   appName: 'Tinder⁺⁺',
-  buildType: 'timestamped',
   appVersion: '1.0.0',
   winIco: 'icons/win.ico',
   macIcns: 'icons/mac.icns'
 });
 
-// Log stuff you want
-nw.on('log',  console.log);
+nw.on('log', console.log);
 
-// Build returns a promise
 nw.build().then(function () {
   console.log('all done!');
 }).catch(function (error) {
   console.error(error);
-});
-
-// And supports callbacks
-nw.build(function(err) {
-  if(err) console.log(err);
 });
